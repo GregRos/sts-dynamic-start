68ce02eda2677b7f8409e28b0970165f033c3cb3
GregRos <life.gregr@gmail.com>
2019-03-16 17:51:50 +0200

    Removed some debugging stuff

--- site/app/ts/ui/mount.tsx
+++ site/app/ts/ui/mount.tsx
@@ -5,7 +5,6 @@ import {Api} from "../api/api";
 import {CmpLayoutRoot} from "./cmps/cmp-layout-root";
 async function run() {
     let cards = await Api.getCardData();
-    (window as any).asad = cards;
     ReactDOM.render(<CmpLayoutRoot cards={cards}/>, document.getElementById("root"))
 }
 
